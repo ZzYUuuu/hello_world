e7783619f5f3c54f2514d08b5876fadb17c5e37f
Yu <72421731+ZzYUuuu@users.noreply.github.com>
2020-10-27 11:16:48 +0800

    Update pipelines.py

--- hello_world/scrapy/scholarship/scholarship/pipelines.py
+++ hello_world/scrapy/scholarship/scholarship/pipelines.py
@@ -7,20 +7,20 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import json
-import openpyxl
+import openpyxl#使用xls来保存数据
 
 class ScholarshipPipeline:
-    def __init__(self):
+    def __init__(self):#初始化条件
         # self.f = open("URL.json","wb")
         self.spider = None
         self.count = 0
 
-    def open_spider(self, spider):
+    def open_spider(self, spider):#初始化xls的页面
         self.wb = openpyxl.Workbook()
         self.ws = self.wb.active
-        self.ws.append(['title', 'url'])
+        self.ws.append(['title', 'url'])#所包含的两个value值的key
 
-    def process_item(self, item, spider):
+    def process_item(self, item, spider):#存入数据
         # content = json.dumps(dict(item),ensure_ascii=False) + ","
         # self.f.write(content.encode())
         # return item
@@ -28,6 +28,6 @@ class ScholarshipPipeline:
         self.ws.append(line)
         return item
 
-    def close_spider(self,spider):
+    def close_spider(self,spider):#关闭spider
         # self.f.close()
-        self.wb.save("geturl.xlsx")
+        self.wb.save("geturl.xlsx")#保存数据
